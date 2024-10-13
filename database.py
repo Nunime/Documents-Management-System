79b78ae6cbbb596918a3682f0848a854d08ef168
nunime <gavrikov.ns@it-psg.com>
2024-10-13 20:11:02 +0300

    13.10.24

--- database.py
+++ database.py
@@ -1,18 +1,27 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base, registry
 
-# URL подключения к базе данных PostgreSQL
+# Создаем объект registry, который будет использоваться для конфигурации моделей
+mapper_registry = registry()
+
+# Создаем базовый класс для моделей
+Base = mapper_registry.generate_base()
+
+# Настраиваем подключение к базе данных
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/document_management_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Создаем фабрику сессий
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
+# Функция для получения сессии
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+# Настройка всех мапперов после объявления моделей
+mapper_registry.configure()
